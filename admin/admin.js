bd02bc7a9713d8c448c514cc3d4af29e08c2bc89
Vedanexus <singh.ajay488@gmail.com>
2026-01-28 16:06:23 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,10 +1,15 @@
-const list = document.getElementById("list");
-
-function saveProduct(){
-  const title = document.getElementById("title").value;
-  const price = document.getElementById("price").value;
-
-  const item = document.createElement("li");
-  item.innerText = title + " - ₹" + price;
-  list.appendChild(item);
+async function addProduct() {
+  await fetch("https://vedanexus-backend.onrender.com/api/products", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer YOUR_ADMIN_TOKEN"
+    },
+    body: JSON.stringify({
+      name: name.value,
+      price: price.value,
+      image: image.value
+    })
+  });
+  alert("Product added");
 }
