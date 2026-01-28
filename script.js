b475eee9619d61a3a85d168874adf10605ae6063
Vedanexus <singh.ajay488@gmail.com>
2026-01-28 16:53:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,35 @@ if (slides.length > 0) {
     slides[currentSlide].classList.add("active");
   }, 4000); // change every 4 seconds
 }
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(index) {
+  slides.forEach(s => s.classList.remove("active"));
+  dots.forEach(d => d.classList.remove("active"));
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+
+  currentSlide = index;
+}
+
+function nextSlide() {
+  let next = (currentSlide + 1) % slides.length;
+  showSlide(next);
+}
+
+function prevSlide() {
+  let prev = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(prev);
+}
+
+function goToSlide(index) {
+  showSlide(index);
+}
+
+/* AUTO SLIDE */
+if (slides.length > 0) {
+  setInterval(nextSlide, 4000);
+}
